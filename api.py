609759705ff056a0be28ba053a226d91cb9a2937
Falanteris <rayhanrts@gmail.com>
2026-03-04 15:02:52 +0700

    Test very vuln

---
+++ api.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, jsonify
+import subprocess
+app = Flask(__name__)
+
+@app.route("/echo", methods=["GET", "POST"])
+def echo():
+    if request.method == "POST":
+        data = request.get_json(silent=True) or {}
+        user_input = data.get("input", "")
+    else:
+        user_input = request.args.get("input", "")
+    
+    parsed = user_input.split(" ")
+
+    return jsonify({"echo": subprocess.check_output(parsed,shell=True,text=True)})
+
+if __name__ == "__main__":
+    app.run(debug=True)
